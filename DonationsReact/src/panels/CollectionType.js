6c39ec53efabfec338dad9b12714d4329afd82b8
ButkovskiyLev <levbut@gmail.com>
2020-09-13 19:48:59 +0500

    First page + Half/second page

--- DonationsReact/src/panels/CollectionType.js
+++ DonationsReact/src/panels/CollectionType.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, PanelHeader, Button, Div } from '@vkontakte/vkui';
+import { Link } from 'react-router-dom'
 
 import '../App.css';
+import './CollectionType.css';
 
 const CollectionType = ({ id }) => (
 	<Panel id={id}>
-		<PanelHeader>Пожертвования</PanelHeader>
-		<Div className = "content">
-				<span id="textHeadline">Выбор типа</span><br/>
-
+		<PanelHeader>Тип сбора</PanelHeader>
+		<Div className="content">
+			<Link to='/type'>
+				<Div className="typeBlock">
+					<div id="title">Создать сбор</div>
+					<div id="subtitle">Когда есть определённая цель</div>
+				</Div>
+			</Link>
+			<Link to='/type'>
+				<Div className="typeBlock">
+					<div id="title">Регулярный сбор</div>
+					<div id="subtitle">Если помощь нужна ежемесячно</div>
+				</Div>
+			</Link>
 		</Div>
 	</Panel>
 );
--- DonationsReact/src/panels/Home.js
+++ DonationsReact/src/panels/Home.js
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 import { Panel, PanelHeader, Button, Div } from '@vkontakte/vkui';
 import '../App.css';
+import './Home.css';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Пожертвования</PanelHeader>
 		<Div className = "content">
-				<span id="textHeadline">У вас пока нет сборов.</span><br/>
-				<span>Начните доброе дело</span>
-					<Div>
+			<Div className = "textContent">
+				<div id="textHeadline">У вас пока нет сборов.</div>
+				<div id="textHeadline">Начните доброе дело.</div>
+				<span></span>
+					<Div className="button">
 						<Link to='/type'>Создать сбор</Link>
 					</Div>
+				</Div>
 		</Div>
 	</Panel>
 );
